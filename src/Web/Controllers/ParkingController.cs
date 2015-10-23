e75412c2152c07381d9c772b5622a6f770ce32a2
folbo <jacunia@op.pl>
2015-10-23 21:55:34 +0200

    Szkielet ParkingControllera.

---
+++ src/Web/Controllers/ParkingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Helpers;
+
+namespace Web.Controllers
+{
+    //todo
+    [pAuthorize(Role = Roles.ParkingOwner)]
+    public class ParkingController : Controller
+    {
+        // GET: Parking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult SetPlaceStatus(PlaceStatus status)
+        {
+            return View();
+        }
+
+        public ActionResult ReservePlace()
+        {
+            return View();
+        }
+    }
+
+    public enum PlaceStatus
+    {
+        Available,
+        Reserved,
+        Disabled
+    }
+}
