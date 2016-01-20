83b8d55827ebcdb004d7b2ab3674c4d4ce28183a
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-01-20 19:15:15 +0100

    dodanie OwnerName do parkingDTO

--- src/Core/Domain/DTO/ParkingDTO.cs
+++ src/Core/Domain/DTO/ParkingDTO.cs
@@ -9,6 +9,7 @@ namespace Ultra.Core.Domain.DTO
         public int TotalPlacesCount { get; set; }
         public int FreePlacesCount { get; set; }
         public Guid OwnerId { get; set; }
+        public string OwnerName { get; set; }
         public double? LocationLatitude { get; set; }
         public double? LocationLongitude { get; set; }
     }
